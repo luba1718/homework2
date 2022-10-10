281856f974e587d1a526e9aae44c9cc5a7f1e692
Luba <bart6891@ya.ru>
2022-10-10 12:33:39 +0300

    num2.1

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введи число: ");
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Write("Введи число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 string NumberText = Convert.ToString(Number);
 if (NumberText.Length > 2)
